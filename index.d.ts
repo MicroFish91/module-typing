649d2468901db649fbdc7fe9539b133df9747ddb
MicroFish91 <mwfisher91@gmail.com>
2022-04-03 15:37:42 -0700

    Initial setup

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module lodash {
+  export function partition(...args: any[]): any;
+}
+
+export = lodash;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import _ from "lodash";
+
+_.partition([1, 2, 3, 4], (n) => n % 2);
